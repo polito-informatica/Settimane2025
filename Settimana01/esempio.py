4c2f1e418e11f785bb7226f220c627e219206a45
Fulvio Corno <fulvio.corno@polito.it>
2025-09-24 14:26:48 +0200

    esempi

---
+++ Settimana01/esempio.py
@@ -0,0 +1,10 @@
+print("Hello, world!") 
+print('Hello again...')
+print("l'oggi")
+print(3)
+print(4+2)  # espressione aritmetica tra numeri interi
+print('4+2') # costante di tipo stringa
+print(4+2*3)
+print(1/3)
+print( "abc" + "def" )
+# print('abc' / "def" )
---
+++ Settimana01/esempio2.py
@@ -0,0 +1,24 @@
+a = 5
+b = 7
+c = 'ciao'
+pi = 3.14
+
+anno = 2025
+wk3 = 2025
+
+print(a)
+
+a = 10
+
+print(a-b)
+
+b = b + 1 # incrementa b
+
+# ? = b+1
+# ? = 7+1
+# ? = 8
+# b = 8
+
+print(b)
+
+print(a+pi)
