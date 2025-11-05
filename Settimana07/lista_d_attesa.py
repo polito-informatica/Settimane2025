18c633d7876e141d5a377cdf3e24fbf26a6ffe34
bardini <roberta.bardini@polito.it>
2025-11-05 16:17:46 +0100

    +esercizio sulle liste - lista d'attesa

--- Settimana07/lista_d_attesa.py
+++ Settimana07/lista_d_attesa.py
@@ -21,3 +21,77 @@ Dopo ogni iterazione, il programma restituisce in output la lista d'attesa nel s
 Il programma termina quano viene inserita in input la stringa vuota ("").
 
 """
+
+persone = ["marco", "anna", "andrea", "elisa","elisa"]
+
+operazione = input("Inserisci operazione: ")
+
+while operazione != "":
+
+    # variabile booleana per validare l'input
+    input_valido = True
+
+    parti = operazione.split()
+
+    if len(parti)!=2:
+        print("Input non valido: operazione mal strutturata")
+        input_valido = False
+
+    if input_valido:
+        azione = parti[0].upper()
+        persona = parti[1].lower()
+
+        if azione not in ["P", "A", "I", "R"]: #"PAIR": 
+            print("Input non valido: azione inesistente")
+            input_valido = False
+
+    if input_valido:
+
+        if azione == "P":
+            if persona in persone:
+                print(f"La posizione di {persona} è {persone.index(parti[1])+1}")
+                """
+                # VERSIONE CHE GESTISCE GLI OMONIMI
+                posizioni = []
+                for i, p in enumerate(persone):
+                    if p == persona:
+                        posizioni.append(i)
+                
+                print(f"La posizione di {persona} è {posizioni}")
+                """
+            else:
+                print(f"Non posso stampare la posizione perché {persona} non è in lista.")
+
+        elif azione == "A":
+            """
+            # VERSIONE CHE AMMETTE GLI OMONIMI
+            persone.append(parti[1])
+            """
+            if persona not in persone:
+                persone.append(parti[1])
+            else: 
+                print(f"Non posso aggiungere {persona} perché è già in lista.")
+
+        elif azione == "R":
+            if persona in persone:
+                persone.remove(persona)
+            else:
+                print(f"Non posso rimuovere {persona} perché non è in lista.")
+        
+        elif azione == "I":
+            """
+            # VERSIONE CHE AMMETTE GLI OMONIMI
+            persone.insert(0, persona)
+            """     
+            if persona not in persone:
+                persone.insert(0, persona)
+            else:
+                print(f"Non posso inserire {persona} perché è già in lista.")
+            
+        # stampa del contenuto corrente della lista
+        print()
+        for i, p in enumerate(persone):
+            print(i+1, p)
+    
+    operazione = input("Inserisci operazione: ")
+ 
