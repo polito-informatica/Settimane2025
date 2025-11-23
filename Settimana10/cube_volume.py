3a33445c6bb5332a7397ffe4e35c08cbf4cfd2f2
Fulvio Corno <fulvio.corno@polito.it>
2025-11-23 20:11:33 +0100

    Esempietti sulle funzioni

---
+++ Settimana10/cube_volume.py
@@ -0,0 +1,9 @@
+def cube_volume(side_length):
+    volume = side_length ** 3
+    return volume
+    
+a = 5
+vol = cube_volume(a)
+vol2 = cube_volume(2*a)
+
+print(vol)
---
+++ Settimana10/multiply.py
@@ -0,0 +1,7 @@
+def multiply(values, factor) :
+    for i in range(len(values)) :
+        values[i] = values[i] * factor
+
+scores = [ 3, 7, 11, 2, 9 ]
+
+multiply(scores, 3)
